e16e3ef6e641c93a413ca55e13ac5606d888d5b8
CasualR <kolyabaner@gmail.com>
2025-11-11 22:01:51 +0100

    server and database update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-console.log('hello world')
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+// Middleware
+app.use(express.json())
+
+// Tells express to serve all files from the public folder as static assets / file.
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
