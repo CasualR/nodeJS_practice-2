fa4ddc1e07aabb7ca87389224a618cf5eae31c7e
CasualR <kolyabaner@gmail.com>
2026-01-18 20:50:25 +0100

    update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -25,16 +25,17 @@ router.post('/register', async (req, res) => {
 
     // now that we have a user, I want to add their first todo for them
     const defaultTodo = `Hello :) Do not add your first todo!`
-    // await prisma.todo.create({
-    //   data: {
-    //     task: defaultTodo,
-    //     userId: user.id,
-    //     password: password
-    //   }
-    // })
+    await prisma.todo.create({
+      data: {
+        task: defaultTodo,
+        userId: user.id,
+        password: password
+      }
+    })
+    
     // create a token
-    // const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
-    // res.json({ token })
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+    res.json({ token })
   } catch (err) {
     console.log(err.message)
     res.sendStatus(503)
