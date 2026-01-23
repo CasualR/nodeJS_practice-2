34f9329cb6d8ae8caeaf030e437478c6ccdb2e8f
CasualR <kolyabaner@gmail.com>
2026-01-23 23:57:01 +0100

    афа

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -32,8 +32,6 @@ router.post('/register', async (req, res) => {
         password: password
       }
     })
-
-    // Not an exception
     
     // create a token
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
