c704318d6f2386ba4fc2705ee845d271de4026c6
Kadzual <kolyabaner@gmail.com>
2026-05-19 22:17:52 +0200

    f

--- src/server.js
+++ src/server.js
@@ -38,7 +38,6 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes)
 app.use('/todos', authMiddleware, todoRoutes)
 
-
 app.listen(PORT, () => {
 
     console.log(`Server has started on port as well: ${PORT}`)
